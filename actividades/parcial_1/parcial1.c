d0bfa5c827a019b5479062838bac2b36bc93d386
daespa1998-beep <daespa1998@gmail.com>
2025-09-17 15:55:42 -0500

    usando typedef todavia tiene un error

---
+++ actividades/parcial_1/parcial1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+typedef  {
+    int codigo;
+    float nota;
+} Estudiante;
+
+Estudiante estud[]= {
+    {1235138975, 4.5},
+    {2545555282, 2.5},
+    {5841641445, 3.4},
+    {6516146661, 1.5},
+    {1446511654, 5.0}
+};
