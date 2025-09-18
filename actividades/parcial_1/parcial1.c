6009e4f37e939b3af5db78b1f56032cc7f37613b
daespa1998-beep <daespa1998@gmail.com>
2025-09-18 17:32:18 -0500

    guardado por fallas del comp  ya se agrego promedio y opcion falta correr codigo para comprobar

--- actividades/parcial_1/parcial1.c
+++ actividades/parcial_1/parcial1.c
@@ -7,9 +7,9 @@ typedef struct {
 // datos de los estudiante 
 Estudiante estud[]= {
     {1235138975, 4.5},
-    {2545555282, 2.5},
-    {1255613221, 3.4},
-    {2154653445, 1.5},
+    {2545555, 2.5},
+    {12556132, 3.4},
+    {215465344, 1.5},
     {1446511654, 5.0}
 };
 int n = 5;   
@@ -21,7 +21,12 @@ float notaMax(int i){
     return (estud[i] .nota > max) ?  estud[i] .nota: max; 
     //? if-else // esta funcion calcula la nota maxima
 }
-
+// promedio
+float sumaNotas(int i){
+    if (i == n- 1) return estud[i] .nota;
+    return estud[i] .nota + sumaNotas(i + 1);
+    // esta funcion calcula la suma de las notas
+}
 
 
 // menu de opciones
@@ -30,13 +35,22 @@ int main() {
     do {
         printf("\n  Menu  \n");
         printf("1. Nota Maxima \n");
+        printf("2. Promedio \n");
+        printf("0. Salir \n");
         printf("elige opcion: ");
         scanf("%d", &opcion); // lee la opcion
 
         if (opcion == 1) {
             printf("la nota maxima es: %.1f\n", notaMax(0));
         }
-    } while (opcion != 0); 
+        else if (opcion == 2) {
+            float promedio = sumaNotas(0)/(float)n;
+            printf("el promedio es: %.2f\n", promedio);
+        }
+        else if (opcion != 0) {
+            printf("Opcion no valida. Intente de nuevo.\n");
+        }
+    } while (opcion != 2); 
     return 0; // fin 
 }                                     
 
