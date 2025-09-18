66aa26c08ae6e5f793c174cbfc4ae4e8790208b2
daespa1998-beep <daespa1998@gmail.com>
2025-09-18 11:48:31 -0500

    agrego opcion 1 para verificar la funcion de recurcibidad de la nota maxima y error corregido

--- actividades/parcial_1/parcial1.c
+++ actividades/parcial_1/parcial1.c
@@ -8,18 +8,35 @@ typedef struct {
 Estudiante estud[]= {
     {1235138975, 4.5},
     {2545555282, 2.5},
-    {5841641445, 3.4},
-    {6516146661, 1.5},
+    {1255613221, 3.4},
+    {2154653445, 1.5},
     {1446511654, 5.0}
 };
 int n = 5;   
 // puse la cantidad de estudiantes 
-// funciones
-//1 nota maxima
+// funciones,  nota maxima
 float notaMax(int i){
-    if (i == n- 1)return
-estud[i] .nota;
+    if (i == n- 1)return estud[i] .nota;
     float max =notaMax (i + 1);
-    return (estud[i] .nota > max) ?
-estud[i] .nota: max;
+    return (estud[i] .nota > max) ?  estud[i] .nota: max; 
+    //? if-else // esta funcion calcula la nota maxima
 }
+
+
+
+// menu de opciones
+int main() {
+    int opcion; 
+    do {
+        printf("\n  Menu  \n");
+        printf("1. Nota Maxima \n");
+        printf("elige opcion: ");
+        scanf("%d", &opcion); // lee la opcion
+
+        if (opcion == 1) {
+            printf("la nota maxima es: %.1f\n", notaMax(0));
+        }
+    } while (opcion != 0); 
+    return 0; // fin 
+}                                     
+
