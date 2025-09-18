79c16ae635ee437816e637a80ad0af5d0af79bd3
daespa1998-beep <daespa1998@gmail.com>
2025-09-17 23:12:37 -0500

    se inicio la funcione(nota max)

--- actividades/parcial_1/parcial1.c
+++ actividades/parcial_1/parcial1.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-typedef  {
+//estrutura que estamos def
+typedef struct {
     int codigo;
     float nota;
 } Estudiante;
-
+// datos de los estudiante 
 Estudiante estud[]= {
     {1235138975, 4.5},
     {2545555282, 2.5},
@@ -11,3 +12,14 @@ Estudiante estud[]= {
     {6516146661, 1.5},
     {1446511654, 5.0}
 };
+int n = 5;   
+// puse la cantidad de estudiantes 
+// funciones
+//1 nota maxima
+float notaMax(int i){
+    if (i == n- 1)return
+estud[i] .nota;
+    float max =notaMax (i + 1);
+    return (estud[i] .nota > max) ?
+estud[i] .nota: max;
+}
